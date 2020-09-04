9b09f752a24ce0c4475281b4760d6dca59a51873
Jaimico <jaimico@Jaimico-Notebook.mjs>
2020-09-04 18:51:19 -0300

    Agrego nuevo componente para controlar el proceso de pedido. Importo los modulos de Materia e instalo bootstrap.

---
+++ Practico/Trabajos_Evaluables/TP_06/TP6-ISW-GRUPO2/src/app/generate-order/generate-order.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-generate-order',
+  templateUrl: './generate-order.component.html',
+  styleUrls: ['./generate-order.component.css']
+})
+export class GenerateOrderComponent implements OnInit {
+
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
+
+}
